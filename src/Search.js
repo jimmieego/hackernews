a0a187ca8d53a395c6e38f772cad05f880776f30
Tao Zhang <jimmieboy@gmail.com>
2019-06-10 16:22:09 -0700

    Add ref

--- src/Search.js
+++ src/Search.js
@@ -1,10 +1,19 @@
 import React from "react";
 
-const Search = ({ value, onChange, onSubmit, children }) => (
-  <form onSubmit={onSubmit}>
-    <input type='text' value={value} onChange={onChange} />
-    <button type='submit'>{children}</button>
-  </form>
-);
+const Search = ({ value, onChange, onSubmit, children }) => {
+  let input;
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        type='text'
+        value={value}
+        onChange={onChange}
+        ref={el => (this.input = el)}
+      />
+      <button type='submit'>{children}</button>
+    </form>
+  );
+};
 
 export default Search;
