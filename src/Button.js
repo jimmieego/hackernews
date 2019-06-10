8ded8a3d4ca6cf57cd21180e65cf2c9ae61aaa49
Tao Zhang <jimmieboy@gmail.com>
2019-06-10 13:35:01 -0700

    Define proptype

--- src/Button.js
+++ src/Button.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Button = ({ onClick, className = "", children }) => {
   return (
@@ -6,6 +7,12 @@ const Button = ({ onClick, className = "", children }) => {
       {children}
     </button>
   );
+};
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired
 };
 
 export default Button;
